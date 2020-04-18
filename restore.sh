36c1a0b22f185c4aaa9bc2a0e8d7cf92c3c9151b
josevega96 <jdelavega36@gmail.com>
2020-04-18 18:49:06 -0500

    Initial commit

---
+++ restore.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+## Restore script for my i3 configuration
+
+echo "cloning backup repo"
+
+cd ~
+
+touch .gitignore
+
+echo ".cfg" >> .gitignore
+
+git clone --bare -b Linux-i3 https://github.com/josevega96/dotfiles $HOME/.cfg
+
+echo "Setting up bare git repo"
+
+ echo "found .bashrc writing bareconf alias"
+ echo "# bare alias" >> .bashrc
+  echo "alias bareconf='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> .bashrc
+         source ~/.bashrc
+
+bareconf checkout
+
+echo "you can now access your home repository using \"bareconf\""
+
+echo "requesting sudo access you only need to type tour password once"
+
+sudo sh -c "echo 'Defaults        timestamp_timeout=-1' >> /etc/sudoers"
+
+echo "enabling multilib repos"
+
+sudo sed -i 's|#[multilib]|[multilib]|g' /etc/pacman.conf
+
+sudo sed -i '93s|#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|g'  /etc/pacman.conf
+
+echo "updating pacman database" 
+
+sudo pacman -Sy
+
+echo "installing all packages found in .config/pkgbabackup/pkglist.txt"
+
+sudo pacman -S --needed - < /home/$USER/.config/pkgbackup/pkglist.txt
+
+echo "creating user dirs"
+
+xdg-user-dirs-update
+
+echo "installing yay"
+
+git clone https://aur.archlinux.org/yay.git ~/yay 
+
+cd ~/yay 
+
+makepkg -si
+
+cd 
+
+rm yay
+
+echo "installing all packages from the AUR"
+
+yay -S --needed  - < /home/$USER/.config/pkgbackup/pkglist-aur.txt
+
+echo "creating pacman hook for pkgbackup"
+
+echo "[Trigger]
+ Operation = Install 
+Operation = Remove 
+Type = Package 
+Target = *
+
+[Action] 
+When = PostTransaction 
+Exec = /bin/sh -c '/usr/bin/pacman -Qqen > /home/$USER/.config/pkgbackup/pkglist.txt'" | sudo tee /etc/pacman.d/hooks/pkgbackup.hook
+
+
+echo "[Trigger]
+ Operation = Install 
+Operation = Remove 
+Type = Package 
+Target = *
+
+[Action] 
+When = PostTransaction 
+Exec = /bin/sh -c '/usr/bin/pacman -Qqem > /home/$USER/.config/pkgbackup/pkglist-aur.txt'" | sudo tee /etc/pacman.d/hooks/pkgbackup-aur.hook
+
+echo "setting up reflector" 
+
+sudo touch /etc/systemd/system/reflector.service
+
+sudo sh -c "echo '[Unit] 
+Description=Pacman mirrorlist update 
+Wants=network-online.target 
+After=network-online.target 
+
+[Service] 
+Type=oneshot ExecStart=/usr/bin/reflector --protocol https --latest 30 --number 20 --sort rate --save /etc/pacman.d/mirrorlist
+
+[Install] 
+RequiredBy=multi-user.target' >> /etc/systemd/system/reflector.service"
+
+touch /etc/systemd/system/reflector.timer
+
+sudo sh -c "echo '[Unit] 
+Description=Run reflector weekly 
+
+[Timer] 
+OnCalendar=Mon *-*-* 7:00:00 
+RandomizedDelaySec=15h 
+Persistent=true 
+
+[Install] 
+WantedBy=timers.target' >>  /etc/systemd/system/reflector.timer"
+
+
+echo "preparing to setup keyboad for x please type your keyboard layout"
+
+read kb_lay
+
+echo "Section \"InputClass\" 
+Identifier \"system-keyboard\" 
+MatchIsKeyboard \"on\" 
+Option \"XkbLayout\" \"$kb_lay\" 
+Option \"XkbModel\" \"pc104\" 
+Option \"XkbVariant\" \"qwerty\" 
+Option \"XkbOptions\" \"grp:alt_shift_toggle\" 
+EndSection " | sudo tee  /etc/X11/xorg.conf.d/00-keyboard.conf
+
+echo "setting up automatic timezone"
+
+echo '#!/bin/sh                                 
+case "$2" in
+    up)
+        timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
+    ;;
+esac' | sudo tee /etc/NetworkManager/dispatcher.d/09-timezone
+
+echo "ading user $USER to the video group"
+
+sudo usermod -a -G video $USER
+
+echo "enabling all necessary systemd services"
+
+sudo systemctl enable ly.service 
+
+sudo systemctl enable libvirt.service 
+
+sudo systemctl start reflector.timer
+
+sudo sed -i 's|Defaults        timestamp_timeout=-1| |g' /etc/sudoers
+
+echo "finished installing rebooting"
+
+sleep 3
+
+reboot
